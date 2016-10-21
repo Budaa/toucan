dc32a146d69d38c29093ebe90f97c2ebbbca587c
Budaa <pbuderaski@gmail.com>
2016-10-21 19:55:05 +0100

    API result caching added

--- api/schoolMembers.php
+++ api/schoolMembers.php
@@ -4,6 +4,7 @@ include 'classes/Config.php';
 include 'classes/Member.php';
 $Config = new Config;
 $return = [];
+$school_id = $_GET['school'];
 
 //Conncecting to database
 $conn = new mysqli($Config->host, $Config->username, $Config->password, $Config->db);
@@ -14,7 +15,7 @@ if ($conn->connect_error) {
 }
 
 //Query to get names of
-$sql = "SELECT member.id, member.name, member.email FROM school_member INNER JOIN member ON school_member.member_id  = member.id WHERE school_member.school_id = 1 ORDER BY member.name ASC";
+$sql = "SELECT member.id, member.name, member.email FROM school_member INNER JOIN member ON school_member.member_id  = member.id WHERE school_member.school_id = $school_id ORDER BY member.name ASC";
 $result = $conn->query($sql);
 
 //IF there is no rows
--- js/script.js
+++ js/script.js
@@ -9,12 +9,32 @@ console.log(api)
 
 
 
-let oReq = new XMLHttpRequest();
-oReq.onload = function (e) {
-    var result = JSON.parse(e.target.response)
-    console.log(result)
 
+var result;
+var school_id = 2;
+//Query cache storage
+var cache = [];
+//Session data
+var session = [];
+//checking if there is any session cache
+if(sessionStorage.getItem('cache')){
+  session = JSON.parse(sessionStorage.getItem('cache'));
 }
-oReq.open('GET',api + 'schoolMembers.php', true);
-
-oReq.send();
+//Checking if there is specific record in chache
+if(typeof session[school_id] !== 'undefined'){
+  result = session[school_id];
+  console.log("data from cache: ");
+  console.log(result);
+}else{
+  //IF not connect to API, get fresh data and save it to the session
+  var oReq = new XMLHttpRequest();
+  oReq.onload = function (e) {
+      result = JSON.parse(e.target.response)
+      cache[school_id] = result;
+      sessionStorage.setItem('cache', JSON.stringify(cache));
+      console.log("This is fresh data: ");
+      console.log(result);
+  }
+  oReq.open('GET',api + 'schoolMembers.php?school=' + school_id, true);
+  oReq.send();
+}
