386d904dd865868114a1e39a4bee159937043671
Budaa <pbuderaski@gmail.com>
2016-10-25 01:39:58 +0100

    Added clousers for function

--- js/script.js
+++ js/script.js
@@ -4,51 +4,103 @@ var local = document.location.href,
     api = local + 'api/';
 
 
+//GETING members for choosen school
+(function(){
+  var result;
+  var school_id = 2;
 
-var result;
-var school_id = 2;
+  //Query cache storage
+  var cache = [];
 
-//Query cache storage
-var cache = [];
+  //Session data
+  var session = [];
 
-//Session data
-var session = [];
-
-//checking if there is any session cache
-if(sessionStorage.getItem('cache')){
-  session = JSON.parse(sessionStorage.getItem('cache'));
-}
-
-//Checking if there is specific record in chache
-if(typeof session[school_id] !== 'undefined'){
-  //Use session storage to define result
-  result = session[school_id];
-
-
-  console.log("data from cache: ");
-  console.log(result);
-}else{
-  //IF not connect to API, get fresh data and save it to the session
-  //Create XMLHR object
-  var oReq = new XMLHttpRequest();
-
-  //Onload function
-  oReq.onload = function (e) {
-    //Parse result to JSON
-    result = JSON.parse(e.target.response)
-
-    //Set cache variable
-    cache[school_id] = result;
-
-    //Add cachef result to sessionStorage
-    sessionStorage.setItem('cache', JSON.stringify(cache));
-
-    //Display result
-    console.log("This is fresh data: ");
-    console.log(result);
+  //checking if there is any session cache
+  if(sessionStorage.getItem('cache')){
+    session = JSON.parse(sessionStorage.getItem('cache'));
   }
-  //Open API url
-  oReq.open('GET',api + 'schoolMembers.php?school=' + school_id, true);
-  //Send request
-  oReq.send();
-}
+
+  //Checking if there is specific record in chache
+  if(typeof session[school_id] !== 'undefined'){
+    //Use session storage to define result
+    result = session[school_id];
+
+
+    console.log("data from cache: ");
+    console.log(result);
+  }else{
+    //IF not connect to API, get fresh data and save it to the session
+    //Create XMLHR object
+    var oReq = new XMLHttpRequest();
+
+    //Onload function
+    oReq.onload = function (e) {
+      //Parse result to JSON
+      result = JSON.parse(e.target.response)
+
+      //Set cache variable
+      cache[school_id] = result;
+
+      //Add cachef result to sessionStorage
+      sessionStorage.setItem('cache', JSON.stringify(cache));
+
+      //Display result
+      console.log("This is fresh data: ");
+      console.log(result);
+    }
+    //Open API url
+    oReq.open('GET',api + 'schoolMembers.php?school=' + school_id, true);
+    //Send request
+    oReq.send();
+  }
+}());
+
+//Geting school list
+(function(){
+  var result;
+
+  //Query cache storage
+  var cache;
+
+  //Session data
+  var session;
+
+  //checking if there is any session cache
+  if(sessionStorage.getItem('schoolList')){
+    session = JSON.parse(sessionStorage.getItem('schoolList'));
+  }
+
+  //Checking if there is specific record in chache
+  if(typeof session !== 'undefined'){
+    //Use session storage to define result
+    result = session;
+
+
+    console.log("data from cache: ");
+    console.log(result);
+  }else{
+    //IF not, connect to API, get fresh data and save it to the session
+    //Create XMLHR object
+    var oReq = new XMLHttpRequest();
+
+    //Onload function
+    oReq.onload = function (e) {
+      //Parse result to JSON
+      result = JSON.parse(e.target.response)
+
+      //Set cache variable
+      cache = result;
+
+      //Add cachef result to sessionStorage
+      sessionStorage.setItem('schoolList', JSON.stringify(cache));
+
+      //Display result
+      console.log("This is fresh data: ");
+      console.log(result);
+    }
+    //Open API url
+    oReq.open('GET',api + 'schoolList.php', true);
+    //Send request
+    oReq.send();
+  }
+}())
